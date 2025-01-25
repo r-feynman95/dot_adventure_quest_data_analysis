10fc995ba65ade5800af99141c65b66dd43b75c4
r-feynman95 <62044927+r-feynman95@users.noreply.github.com>
2025-01-25 14:39:11 -0500

    structuring database
    setting up the database that data_clean will pass cleaned objects too

--- application/models.py
+++ application/models.py
@@ -10,3 +10,17 @@ class IncomeExpenses(db.Model):                                                 
 
     def __str__(self):
         return self.id     # when an object of this class is printed as a string use the primary key id
+    
+# Database model for cleaned log file
+class Log(db.Model):
+    user = db.Column(db.Integer, primary_key = True)
+    start = db.Column(db.DateTime, nullable = False)
+    duration = db.Column(db.Integer, nullable = False)
+    seed = db.Column(db.Integer, nullable = False)
+    problem_number = db.Column(db.Integer, nullable = False)
+    problem_type = db.Column(db.String(8), nullable = False)
+    problem_status = db.Column(db.String(50), nullable = False)
+    response = db.Column(db.String(100), nullable = True)
+
+    def __str__(self):
+        return self.user     # when an object of this class is printed as a string use the primary key id
